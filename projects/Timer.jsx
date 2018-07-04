01ed7d999360823d5d11c005c9c161d1e523ae4d
Daniel <sychoi4880@gmail.com>
2018-07-04 15:41:48 +0900

    shouldComponentUpdate function

--- projects/Timer.jsx
+++ projects/Timer.jsx
@@ -3,32 +3,52 @@ import moment from 'moment';
 import 'moment/locale/ko';
 moment.locale('ko');
 
+const TIME_FORMAT = 'A h:mm';
+
 class Timer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       date: moment(),
-      expireDate: moment(props.expireDate),
     };
 
-    this.nTimer = setInterval(this.tick, 1000);
+    this.nTimer = setInterval(() => {
+      this.setState({
+        date: moment(),
+      });
+    }, 1000);
   }
 
-  tick = () => {
-    console.log('1');
-    if (moment.duration(moment().diff(this.state.expireDate)).asSeconds() > 0) {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.state.date.format(TIME_FORMAT) === nextState.date.format(TIME_FORMAT)
+    )
+      return false;
+    else return true;
+  }
+
+  /*tick = () => {
+    if (moment() > moment(this.props.expireDate)) {
       alert('종료되었습니다.');
       clearInterval(this.nTimer);
     }
     this.forceUpdate();
-  };
+  };*/
 
   render() {
+    console.log('render');
+    const { expireDate } = this.props;
+    const { date } = this.state;
+
+    if (moment(expireDate) < date) {
+      return <div>종료 되었습니다.</div>;
+    }
+
     return (
       <div>
-        <div>현재시간은 {moment().format('A h:mm')} </div>
-        <div>{this.state.expireDate.fromNow()}에 강의 종료합니다.</div>
+        <div>현재시간은 {date.format(TIME_FORMAT)} </div>
+        <div>{moment(expireDate).fromNow()}에 강의 종료합니다.</div>
       </div>
     );
   }
